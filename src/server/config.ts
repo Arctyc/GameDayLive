1a9c93f4530a711e432c5e0c411067ba9c626176
Arctyc <arctycdev@gmail.com>
2026-01-13 17:07:55 -0500

    Change config redis key

--- src/server/config.ts
+++ src/server/config.ts
@@ -8,7 +8,7 @@ const keyFor = (subreddit: string) => `subreddit:${subreddit}`;
 export async function setSubredditConfig(subredditName: string, config: SubredditConfig): Promise<void> {
   const logger = await Logger.Create('Config - Set');
   
-  const key = keyFor(context.subredditId);
+  const key = keyFor(subredditName);
   await redis.set(key, JSON.stringify(config));
   logger.info(`Saved config for ${subredditName}:`, config);
 }
@@ -17,15 +17,15 @@ export async function setSubredditConfig(subredditName: string, config: Subreddi
 export async function getSubredditConfig(subredditName: string): Promise<SubredditConfig | undefined> {
   const logger = await Logger.Create('Config - Get');
   
-  const key = keyFor(context.subredditId);
+  const key = keyFor(subredditName);
   const data = await redis.get(key);
 
   if (!data) {
-    logger.info(`No config found for ${subreddit}`);
+    logger.info(`No config found for ${subredditName}`);
     return undefined;
   }
   
   const config = JSON.parse(data) as SubredditConfig;
-  logger.info(`Retrieved config for ${subreddit}:`, config);
+  logger.info(`Retrieved config for ${subredditName}:`, config);
   return config;
 }

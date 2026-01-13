4e8088eeccc368c36f803eeddd55504e7f0cdb4d
Arctyc <arctycdev@gmail.com>
2026-01-13 13:10:24 -0500

    Update README.md and instructions

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,12 +1,15 @@
 
-export const LEAGUES = ["nhl"] as const; // Add future leagues here
+export const LEAGUES = ["nhl"] as const; // Add future leagues here (1)
 
 export interface SubredditConfig {
   league: typeof LEAGUES[number]; 
   nhl?: NHLConfig;
+  // here (2)
   enablePostgameThreads: boolean;
 }
 
 export interface NHLConfig {
   teamAbbreviation: string;
 }
+
+// and here (3) as necessary

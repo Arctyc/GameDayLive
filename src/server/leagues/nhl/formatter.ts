807badbca4f0dad2ca7bf66accf3aeef1fb6f51b
Arctyc <arctycdev@gmail.com>
2026-01-15 23:19:06 -0500

    Update thread header format

--- src/server/leagues/nhl/formatter.ts
+++ src/server/leagues/nhl/formatter.ts
@@ -126,7 +126,7 @@ async function buildBodyHeader(game: NHLGame, subredditName: string): Promise<st
     const header = `# ${awayTeamPlace} ${awayTeamName} @ ${homeTeamPlace} ${homeTeamName}  
 
 **Scoreboard:** ${awayTeamAbbrev} **${awayScore}** : **${homeScore}** ${homeTeamAbbrev}  
-**Status:** ${periodLabel} - ${timeRemainingDisplay}
+**Status:** ${periodLabel} - ${timeRemainingDisplay}  
 **Start Time:** ${localTime} | **Venue:** ${game.venue.default} | **Networks:** ${networks}  
 **Last Update:** ${new Date().toLocaleString('en-US', { timeZone: timezone })}
 `;

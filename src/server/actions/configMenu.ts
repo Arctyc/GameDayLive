c2ce89590f62e2d3ef041d87a890b3f5cec50ef1
Arctyc <arctycdev@gmail.com>
2026-02-27 18:28:05 -0500

    Update pregame enable label

--- src/server/actions/configMenu.ts
+++ src/server/actions/configMenu.ts
@@ -73,7 +73,7 @@ export const menuAction = (router: Router): void => {
                                 {
                                     type: 'boolean',
                                     name: 'pregameEnabled',
-                                    label: 'Enable pre-game threads',
+                                    label: 'Enable pre-game threads (Coming soon)',
                                     defaultValue: config?.pregame?.enabled ?? false,
                                 },
                                 {

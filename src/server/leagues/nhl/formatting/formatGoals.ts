944933d5cd1111df15a3698e847e36314aa253e1
Arctyc <arctycdev@gmail.com>
2026-03-05 15:25:56 -0500

    Update table format

--- src/server/leagues/nhl/formatting/formatGoals.ts
+++ src/server/leagues/nhl/formatting/formatGoals.ts
@@ -35,7 +35,7 @@ export function buildBodyGoals(game: NHLGame): string {
     }
 
     if (!hasAnyGoals) {
-        out += `- | - | - | - | - | - | -\n`;
+        out += `| - | - | - | - | - | - | - | - |\n`;
     }
 
     out += `\n`;
